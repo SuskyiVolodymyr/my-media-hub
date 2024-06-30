7071fa966228b93a805ba70096ca0f5bf8c351c4
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-06-30 15:38:56 +0300

    create user, movie, anime, series, cartoon, genre models & changed admin panel

--- media/admin.py
+++ media/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from media.models import User, Genre, Movie, Anime, Cartoon, Series
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["title", "year_released"]
+    list_filter = ["year_released"]
+    search_fields = ["title"]
+
+
+@admin.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ["title", "year_released", "seasons", "episodes"]
+    list_filter = ["year_released", "seasons", "episodes"]
+    search_fields = ["title"]
+
+
+@admin.register(Cartoon)
+class CartoonAdmin(admin.ModelAdmin):
+    list_display = ["title", "year_released", "seasons", "episodes"]
+    list_filter = ["year_released", "seasons", "episodes"]
+    search_fields = ["title"]
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    list_display = ["title", "year_released", "seasons", "episodes"]
+    list_filter = ["year_released", "seasons", "episodes"]
+    search_fields = ["title"]
+
+
+@admin.register(Genre)
+class SeriesAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+admin.site.register(User, UserAdmin)
--- media/models.py
+++ media/models.py
@@ -1,3 +1,101 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    def __str__(self):
+        return f"{self.username}"
+
+
+class UserMediaDataMixin:
+    rate = models.DecimalField(null=True)
+    status = models.CharField(choices=["watching", "want to watch", "dropped", "finished"])
+    comment = models.TextField()
+
+
+class UserMovieData(models.Model, UserMediaDataMixin):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    movie = models.ForeignKey("Movie", on_delete=models.CASCADE)
+
+
+class UserAnimeData(models.Model, UserMediaDataMixin):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    anime = models.ForeignKey("Anime", on_delete=models.CASCADE)
+
+
+class UserCartoonData(models.Model, UserMediaDataMixin):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    cartoon = models.ForeignKey("Cartoon", on_delete=models.CASCADE)
+
+
+class UserSeriesData(models.Model, UserMediaDataMixin):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    series = models.ForeignKey("Series", on_delete=models.CASCADE)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ManyToManyField(Genre, related_name="movies")
+    year_released = models.IntegerField(null=True)
+    user = models.ManyToManyField(get_user_model(), through=UserMovieData, related_name="movies")
+
+    class Meta:
+        ordering = ("title", )
+
+    def __str__(self):
+        return self.title
+
+
+class Series(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ManyToManyField(Genre, related_name="series")
+    seasons = models.IntegerField(null=True)
+    episodes = models.IntegerField(null=True)
+    year_released = models.IntegerField(null=True)
+    user = models.ManyToManyField(get_user_model(), through=UserSeriesData, related_name="series")
+
+    class Meta:
+        ordering = ("title", )
+        verbose_name_plural = "series"
+
+    def __str__(self):
+        return self.title
+
+
+class Anime(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ManyToManyField(Genre, related_name="anime")
+    seasons = models.IntegerField(null=True)
+    episodes = models.IntegerField(null=True)
+    year_released = models.IntegerField(null=True)
+    user = models.ManyToManyField(get_user_model(), through=UserAnimeData, related_name="anime")
+
+    class Meta:
+        ordering = ("title", )
+        verbose_name_plural = "anime"
+
+    def __str__(self):
+        return self.title
+
+
+class Cartoon(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ManyToManyField(Genre, related_name="cartoons")
+    seasons = models.IntegerField(null=True)
+    episodes = models.IntegerField(null=True)
+    year_released = models.IntegerField(null=True)
+    user = models.ManyToManyField(get_user_model(), through=UserCartoonData, related_name="cartoons")
+
+    class Meta:
+        ordering = ("title", )
+
+    def __str__(self):
+        return self.title
