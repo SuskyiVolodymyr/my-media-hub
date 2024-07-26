437f83144b26effacdc63a79d7ceea56a7bde4c4
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-07-26 14:15:57 +0300

    formated urls.py

--- media/urls.py
+++ media/urls.py
@@ -1,17 +1,35 @@
 from media.url_imports import *
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("user/create", UserCreateView.as_view(), name="user-create"),
-    path("user_movies/", UserMovieListView.as_view(), name="user-movie-list"),
+    path(
+        "",
+        index,
+        name="index"),
+    path(
+        "user/create",
+        UserCreateView.as_view(),
+        name="user-create"),
+    path(
+        "user_movies/",
+        UserMovieListView.as_view(),
+        name="user-movie-list"),
     path(
         "user_movie_data/<int:pk>/update/",
         update_user_movie_data_view,
         name="user-movie-data-update"
     ),
-    path("movies/", MovieListView.as_view(), name="movie-list"),
-    path("movies/<int:pk>/add_movie", add_movie, name="movie-add"),
-    path("movie/create", MovieCreateView.as_view(), name="movie-create"),
+    path(
+        "movies/",
+        MovieListView.as_view(),
+        name="movie-list"),
+    path(
+        "movies/<int:pk>/add_movie",
+        add_movie,
+        name="movie-add"),
+    path(
+        "movie/create",
+        MovieCreateView.as_view(),
+        name="movie-create"),
     path(
         "movie/<int:pk>/detail/",
         MovieDetailView.as_view(),
@@ -27,15 +45,30 @@ urlpatterns = [
         MovieDeleteView.as_view(),
         name="movie-delete"
     ),
-    path("user_anime/", UserAnimeListView.as_view(), name="user-anime-list"),
+    path(
+        "user_anime/",
+        UserAnimeListView.as_view(),
+        name="user-anime-list"),
     path(
         "user_anime_data/<int:pk>/update/",
         update_user_anime_data_view,
         name="user-anime-data-update"
     ),
-    path("anime/", AnimeListView.as_view(), name="anime-list"),
-    path("anime/<int:pk>/add_anime", add_anime, name="anime-add"),
-    path("anime/create", AnimeCreateView.as_view(), name="anime-create"),
+    path(
+        "anime/",
+        AnimeListView.as_view(),
+        name="anime-list"
+    ),
+    path(
+        "anime/<int:pk>/add_anime",
+        add_anime,
+        name="anime-add"
+    ),
+    path(
+        "anime/create",
+        AnimeCreateView.as_view(),
+        name="anime-create"
+    ),
     path(
         "anime/<int:pk>/detail",
         AnimeDetailView.as_view(),
@@ -61,9 +94,21 @@ urlpatterns = [
         update_user_series_data_view,
         name="user-series-data-update"
     ),
-    path("series/", SeriesListView.as_view(), name="series-list"),
-    path("series/<int:pk>/add_series", add_series, name="series-add"),
-    path("series/create", SeriesCreateView.as_view(), name="series-create"),
+    path(
+        "series/",
+        SeriesListView.as_view(),
+        name="series-list"
+    ),
+    path(
+        "series/<int:pk>/add_series",
+        add_series,
+        name="series-add"
+    ),
+    path(
+        "series/create",
+        SeriesCreateView.as_view(),
+        name="series-create"
+    ),
     path(
         "series/<int:pk>/detail",
         SeriesDetailView.as_view(),
@@ -89,9 +134,21 @@ urlpatterns = [
         update_user_cartoon_data_view,
         name="user-cartoon-data-update"
     ),
-    path("cartoons/", CartoonListView.as_view(), name="cartoon-list"),
-    path("cartoons/<int:pk>/add_cartoon", add_cartoon, name="cartoon-add"),
-    path("cartoon/create", CartoonCreateView.as_view(), name="cartoon-create"),
+    path(
+        "cartoons/",
+        CartoonListView.as_view(),
+        name="cartoon-list"
+    ),
+    path(
+        "cartoons/<int:pk>/add_cartoon",
+        add_cartoon,
+        name="cartoon-add"
+    ),
+    path(
+        "cartoon/create",
+        CartoonCreateView.as_view(),
+        name="cartoon-create"
+    ),
     path(
         "cartoon/<int:pk>/detail",
         CartoonDetailView.as_view(),
